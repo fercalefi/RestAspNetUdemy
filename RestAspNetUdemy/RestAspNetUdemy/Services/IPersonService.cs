b02755f856a940d27379d0e3e30fa8b8e96f1bb0
Fernando Calefi <fercalefi@gmail.com>
2019-11-19 22:58:44 -0300

    Implementando PersonService e mockando dados

---
+++ RestAspNetUdemy/RestAspNetUdemy/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestAspNetUdemy.Model;
+using System.Collections.Generic;
+    
+namespace RestAspNetUdemy.Services
+{
+    interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        Person Update(Person person);
+        List<Person> FindAll();
+        void Delete(long id);
+    }
+}
---
+++ RestAspNetUdemy/RestAspNetUdemy/Services/Implementations/PersonServiceImp.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using RestAspNetUdemy.Model;
+
+namespace RestAspNetUdemy.Services.Implementations
+{
+    public class PersonServiceImp : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+           
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+
+            }
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Fernando" + i,
+                LastName = "Calefi" + i,
+                Address = "Ida Roncolato Nogueira 180" + i,
+                Gender = "Male"
+            };
+
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Fernando",
+                LastName = "Calefi",
+                Address = "Ida Roncolato Nogueira 180",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+    }
+}
